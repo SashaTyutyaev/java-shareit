522cbcc8504c9e3a218a58ccb5e25ae820be2685
SashaTyutyaev <sashatut2002@yandex.ru>
2024-07-01 23:56:54 +0300

    clenaup code

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -6,10 +6,11 @@ import lombok.Data;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Builder
 @Data
-public class UserDto {
+public class UserDto implements Serializable {
     private Integer id;
     @NotBlank
     @NotNull
